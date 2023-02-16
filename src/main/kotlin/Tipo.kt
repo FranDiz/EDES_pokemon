952e79fd5c044f79b6c4299ae56c5ea16b06d575
Fran <Fran_Diz@outlook.com>
2023-02-16 16:53:36 +0100

    ListaTipos

--- src/main/kotlin/Tipo.kt
+++ src/main/kotlin/Tipo.kt
@@ -1,3 +1,16 @@
+import java.lang.IllegalArgumentException
+
 class Tipo(tipo:String) {
-    var tipo =""
+    var tipo = ""
+        get() {
+            return field
+        }
+        set(value) {
+            if (value !in arrayOf("acero", "agua", "bicho", "dragon", "electrico", "fantasma", "fuego", "hada", "hielo", "lucha", "normal", "planta", "psiquico", "roca", "siniestro", "tierra", "veneno", "volador")){
+                throw IllegalArgumentException("El tipo no es válido"); field = ""
+            }
+            else {
+                field=value
+            }
+        }
 }
